4f4bc97d4cc15d33f6c7524d69588146e6f76511
Armin Nel <arminnelmain@protonmail.com>
2021-03-29 11:40:15 +0200

    add basic resolver functionality with axios

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,20 +1,32 @@
 import "reflect-metadata";
 import { Resolver, Query, Arg } from "type-graphql";
-import { Character } from "./types";
+import { Character, Planet } from "./types";
 import axios from "axios";
 
 @Resolver()
 export class CharacterResolver {
   // All characters.
   @Query(() => [Character])
-  async people() {
+  async getAllCharacters() {
     const res = await axios.get("https://swapi.dev/api/people/");
     return res.data;
   }
-  // One character.
+  // Search character by name.
   @Query(() => Character)
-  async person(@Arg("id") id: number) {
-    const res = await axios.get(`https://swapi.dev/api/people/${id}`);
+  async personByName(@Arg("name") name: string) {
+    const res = await axios.get(
+      ` https://swapi.dev/api/people/?search=${name} `
+    );
+    return res.data;
+  }
+}
+
+@Resolver()
+export class PlanetResolver {
+  // All planets.
+  @Query(() => [Planet])
+  async planets() {
+    const res = await axios.get("https://swapi.dev/api/planets/");
     return res.data;
   }
 }
