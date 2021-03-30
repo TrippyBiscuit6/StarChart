5085ea4cad59324fe11788bd47a3a6a50d56c710
Armin Nel <arminnelmain@protonmail.com>
2021-03-30 09:10:38 +0200

    add concurrently script for both server and client

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import * as cors from "cors";
 import { buildSchema } from "type-graphql";
 import { CharacterResolver, PlanetResolver } from "./resolvers";
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 /**
  * Initialize Apollo server and apply express middleware.
